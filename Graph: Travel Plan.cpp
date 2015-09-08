e0355cdcb730bdf55315b8d993a221c298b31f66
xukaituo1994 <xukaituo1994@gmail.com>
2015-09-08 16:11:48 +0800

    Update Graph: Travel Plan.cpp

--- Graph: Travel Plan.cpp
+++ Graph: Travel Plan.cpp
@@ -5,10 +5,12 @@
 
 using namespace std;
 
-const int MAXSIZE = 100;
+const int MAXSIZE = 500;
 struct MGraph {
-	int Edges[MAXSIZE][MAXSIZE];
-	int Costs[MAXSIZE][MAXSIZE];
+	//int Edges[MAXSIZE][MAXSIZE];
+	//int Costs[MAXSIZE][MAXSIZE];
+	int **Edges;
+	int **Costs;
 	int N, E;
 };
 
@@ -17,15 +19,14 @@ void CreateMGraph(MGraph &G, int &s, int &d)
 {
 	int x, y, len, cost;
 	cin >> G.N >> G.E >> s >> d;
+	G.Edges = new int*[G.N];
+	G.Costs = new int*[G.N];
 	for (int i = 0; i != G.N; ++i) {
+		G.Edges[i] = new int[G.N];
+		G.Costs[i] = new int[G.N];
 		for (int j = 0; j != G.N; ++j) {
-			if (i == j) {
-				G.Edges[i][j] = 0;
-				G.Costs[i][j] = 0;
-			} else {
-				G.Edges[i][j] = INF;
-				G.Costs[i][j] = INF;
-			}//here
+			G.Edges[i][j] = INF;
+			G.Costs[i][j] = INF;
 		}
 	}
 	for (int i = 0; i != G.E; ++i) {
@@ -53,8 +54,8 @@ void Dijkstra(MGraph G, int v, int dist[], int path[], int money[])
 	set[v] = 1; path[v] = -1;
 	//key operation
 	for (int i = 0; i != G.N; ++i) {
-		//find vertex u whose distance is shortest 
-		int u, min = INF;
+		//find vertex u whose distance is shortest
+		int u = 0, min = INF;
 		for (int j = 0; j != G.N; ++j) {
 			if (set[j] == 0 && dist[j] < min) {
 				u = j;
@@ -66,7 +67,7 @@ void Dijkstra(MGraph G, int v, int dist[], int path[], int money[])
 		for (int j = 0; j != G.N; ++j) {
 			if (G.Edges[u][j] != INF) {
 				if (set[j] == 0) {
-					if (min + G.Edges[u][j] < dist[j]) {	
+					if (min + G.Edges[u][j] < dist[j]) {
 						dist[j] = min + G.Edges[u][j];	//min = dist[u];
 						path[j] = u;
 						money[j] = money[u] + G.Costs[u][j];
@@ -83,23 +84,23 @@ void Dijkstra(MGraph G, int v, int dist[], int path[], int money[])
 	} //for i
 
 }
-void PrintPath(MGraph G, int path[], int money[], int a)
-{
-	int stack[MAXSIZE], top = -1;
-	int t = a;
-	while (path[a] != -1) {
-		stack[++top] = a;
-		a = path[a];
-	}
-	cout << top + 2 << " " << money[t] << endl;
-}
 
 int main()
 {
 	MGraph G;
 	int s, d;//source, destination
 	int dist[MAXSIZE], path[MAXSIZE], money[MAXSIZE];
+
 	CreateMGraph(G, s, d);
 	Dijkstra(G, s, dist, path, money);
-	PrintPath(G, path, money, d);
+
+	cout << dist[d] << " " << money[d] << endl;
+
+	//free memory
+	for (int i = 0; i != G.N; ++i) {
+		delete[]G.Edges[i];
+		delete[]G.Costs[i];
+	}
+	delete[]G.Edges;
+	delete[]G.Costs;
 }
